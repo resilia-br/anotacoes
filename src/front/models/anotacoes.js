a73193dc0ba0cf58c44329e32c9ec7607b7ea9e4
Daniel-Roch <daniel_roch4@hotmail.com>
2021-05-11 21:10:36 -0300

    Peguei as informacoes

--- src/front/models/anotacoes.js
+++ src/front/models/anotacoes.js
@@ -1,11 +1,17 @@
 class Anotacoes{
     constructor(titulo, notas){
-        this.titulo = titulo;
-        this.notas = notas;
-    }
-    verifica(){
-        if(typeof this.titulo != 'string'){
+        if(typeof titulo != 'string' && typeof notas != 'string'){
             throw new Error ('Criação de notas, dados inválidos')
+        }else{
+            this.titulo = titulo;
+            this.notas = notas; 
         }
     }
 }
+$('.btn').on('click',function(event){
+    event.preventDefault()
+    const recebeAnotacoes = new Anotacoes($('#titulo').val(),$('#descricaoTarefa').val())
+    console.log($('#titulo').val())
+    console.log($('#descricaoTarefa').val())
+    console.log(recebeAnotacoes.titulo, recebeAnotacoes.notas)
+})
