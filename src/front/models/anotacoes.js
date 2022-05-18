7b5d05f82dca6971b6632baa8a666bc86310b742
Julia Prado <juliapbasile@gmail.com>
2022-05-17 21:45:44 -0300

    Ajusta try/catch do anotacoes.js no constructor

--- src/front/models/anotacoes.js
+++ src/front/models/anotacoes.js
@@ -1,17 +1,20 @@
 class Anotacoes {
     constructor(titulo, notas) {
         try {
-            if (typeof this.titulo == String || typeof this.notas == String) {
-                return titulo;
-                return notas;
-            } else if (typeof this.titulo !== String || typeof this.notas !== String) {
+            if (typeof titulo === 'string' && typeof notas == 'string') {
+                this.titulo = titulo;
+                this.notas = notas;
+            } else {
                 throw new Error('Criação de notas - dados inválidos');
             }
         } catch (erro) {
-            console.log(erro);
+            console.error(erro);
+            alert(erro);
         }
+        console.log(`${this.titulo}: ${this.notas}`)
     }
 }
 
 let bloquinho = new Anotacoes(1, 1);
-
+let bloquinho2 = new Anotacoes('b', 1);
+let bloquinho3 = new Anotacoes('b', 'b');
