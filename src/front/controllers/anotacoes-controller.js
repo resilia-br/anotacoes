e680f007959eef827ed598ce443372b584fd7aff
Julia Prado <juliapbasile@gmail.com>
2022-05-19 19:19:20 -0300

    Terminar ouvidor do controller

--- src/front/controllers/anotacoes-controller.js
+++ src/front/controllers/anotacoes-controller.js
@@ -1,13 +1,19 @@
 class AnotacoesController {
     constructor() {
-        this.anotacoesBd = [];
+        this.anotacoesBancoDados = [];
     }
     addAnotacao() {
         const titulo = $('#titulo').val();
         const descricaoTarefa = $('#descricaoTarefa').val();
         const anotacao = new Anotacoes(titulo, descricaoTarefa);
         console.log(anotacao);
-        this.anotacoesBd.push(anotacao);
-        console.log(this.anotacoesBd);
+        this.anotacoesBancoDados.push(anotacao);
+        console.log(this.anotacoesBancoDados);
     }
 }
+const aController = new AnotacoesController()
+
+$('#submit').click((e) => {
+    e.preventDefault();
+    aController.addAnotacao();
+});
